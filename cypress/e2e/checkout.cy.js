4de9e7e8ed4c50f1837b5a7ae769ac5cc39fed2c
Lauper <64489452+LauPerovich@users.noreply.github.com>
2026-05-31 19:17:26 -0300

    Automatiza checkout con datos válidos

---
+++ cypress/e2e/checkout.cy.js
@@ -0,0 +1,25 @@
+describe("Checkout - Sauce Demo", () => {
+  describe("Sesión con standard_user", () => {
+    beforeEach(() => {
+      cy.visit("https://www.saucedemo.com/");
+      cy.get('[data-test="username"]').type("standard_user");
+      cy.get('[data-test="password"]').type("secret_sauce");
+      cy.get('[data-test="login-button"]').click();
+      cy.url().should("include", "/inventory.html");
+
+      cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+      cy.get('[data-test="shopping-cart-link"]').click();
+      cy.url().should("include", "/cart.html");
+      cy.get('[data-test="checkout"]').click();
+      cy.url().should("include", "/checkout-step-one.html");
+    });
+
+    it("Completar checkout con datos válidos", () => {
+      cy.get('[data-test="firstName"]').type("Juan");
+      cy.get('[data-test="lastName"]').type("Pérez");
+      cy.get('[data-test="postalCode"]').type("5000");
+      cy.get('[data-test="continue"]').click();
+      cy.get('[data-test="finish"]').click();
+    });
+  });
+});
